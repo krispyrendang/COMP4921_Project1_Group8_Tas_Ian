050b7a5eb9a51998adbe7960ad79e6d0e8b71649
Ian Jeremy Guani <ianguani@yahoo.ca>
2023-10-03 12:44:02 -0700

    created queries for getting user uploads

--- database/uploads.js
+++ database/uploads.js
@@ -96,13 +96,84 @@ async function linkUpload(postData) {
 	}
 }
 
+async function getUserImage(postData) {
+    let getUserImageSQL = `
+        SELECT long, short, desc
+        FROM uploads
+        WHERE user_id = :user_id AND type = :type
+    `
 
+    let params = {
+		type: postData,
+        user_id: postData
+	}
 
+    try {
+		const results = await database.query(getUserImageSQL, params);
+		console.log("Successfully got user image data");
+		console.log(results[0]);
+		return true;
+	} catch (err) {
+		console.log("Error getting user image data");
+		console.log(err);
+		return false;
+	}
+}
 
+async function getUserText(postData) {
+    let getUserTextSQL = `
+        SELECT long, short, desc
+        FROM uploads
+        WHERE user_id = :user_id AND type = :type
+    `
+
+    let params = {
+		type: postData,
+        user_id: postData
+	}
+
+    try {
+		const results = await database.query(getUserTextSQL, params);
+		console.log("Successfully got user text data");
+		console.log(results[0]);
+		return true;
+	} catch (err) {
+		console.log("Error getting user text data");
+		console.log(err);
+		return false;
+	}
+}
+
+async function getUserLink(postData) {
+    let getUserLinkSQL = `
+        SELECT long, short, desc
+        FROM uploads
+        WHERE user_id = :user_id AND type = :type
+    `
+
+    let params = {
+		type: postData,
+        user_id: postData
+	}
+
+    try {
+		const results = await database.query(getUserLinkSQL, params);
+		console.log("Successfully got user link data");
+		console.log(results[0]);
+		return true;
+	} catch (err) {
+		console.log("Error getting user link data");
+		console.log(err);
+		return false;
+	}
+}
 
 
 module.exports = {
 	imageUpload,
     textUpload,
-    linkUpload
+    linkUpload,
+    getUserImage,
+    getUserText,
+    getUserLink
 };
