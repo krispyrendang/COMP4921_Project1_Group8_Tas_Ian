b10f26265a45b54433689941a6bfe8c75d7aba8b
Ian Jeremy Guani <ianguani@yahoo.ca>
2023-10-04 15:21:49 -0700

    home pages dont have checkboxes / displays now are based on active

--- server.js
+++ server.js
@@ -430,6 +430,10 @@ app.get("/puny/:code", async (req, res) => {
 	}
 });
 
+app.post("/update/active", async (req, res) => {
+	
+})
+
 app.get("/createTables", async (req, res) => {
 	const create_tables = include("database/create_tables");
 
